236feeff8b591be7dcf7326df8380cb6fe698429
Anna Samar <annsummmer@gmail.com>
2026-08-28 15:53:14 +0200

    Add fixes

--- main.py
+++ main.py
@@ -47,4 +47,6 @@ def main() -> QuerySet:
 
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(last_name="Smith").order_by("first_name", flat=True)
+    return (Actor.objects
+            .filter(last_name="Smith")
+            .order_by("first_name", flat=True))
